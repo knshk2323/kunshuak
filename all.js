3d6bcb8fd17c4ac3981a40713fe3e8f9cfb3f862
knshk2323 <kunshuak.06@gmail.com>
2024-10-23 12:13:57 +0500

    Add files via upload

--- all.js
+++ all.js
@@ -25,12 +25,20 @@ document.addEventListener('DOMContentLoaded', function() {
 
     function updateDateTime() {
         const now = new Date();
-        const options = { year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true };
+        const options = {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric',
+            second: 'numeric',
+            hour12: true
+        };
         document.getElementById('currentDateTime').innerText = now.toLocaleString('en-US', options);
     }
-    
-    setInterval(updateDateTime, 1000); // Update every second
-    updateDateTime(); 
+
+    // Событие для обновления времени по кнопке
+    document.getElementById('updateTimeBtn').addEventListener('click', updateDateTime); 
     
     
 
@@ -53,6 +61,65 @@ document.addEventListener('DOMContentLoaded', function() {
                 }
             });
         }); 
+
+    
 });   
 
- 
+document.getElementById('loginForm').addEventListener('submit', function (event) {
+    event.preventDefault(); 
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    // тексеру
+    if (username === 'admin' && password === '12345678') {               
+        const loginModal = new bootstrap.Modal(document.getElementById('loginModal'));
+        loginModal.show();
+
+        // логин пароль тазалайды
+        document.getElementById('username').value = '';
+        document.getElementById('password').value = '';
+    } else {
+        alert('Invalid username or password!');
+    }
+});
+
+
+
+// батырмаға звук
+let button = document.getElementById('themeToggle');
+button.addEventListener('click', () => {
+let sound = new Audio('sound.mp3');
+sound.play();
+});
+
+button1.addEventListener('click', () => {
+    let sound = new Audio('sound.mp3');
+    sound.play();
+ });
+// Load weather on button click
+document.getElementById('getWeatherBtn').addEventListener('click', function() {
+    const apiKey = 'f31f5ea28ceb56f1b59a677de69c9702'; // Your API key
+    const city = 'Astana'; // The city for which to display the weather
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    fetch(url)
+        .then(response => {
+            // Check if the response is successful
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            const weatherInfo = document.getElementById('weatherInfo');
+            weatherInfo.innerHTML = `
+                <h3>Weather in ${data.name}</h3>
+                <p>Temperature: ${data.main.temp}°C</p>
+                <p>Condition: ${data.weather[0].description}</p>
+            `;
+        })
+        .catch(error => {
+            console.error('Error fetching weather data:', error);
+        });
+});
